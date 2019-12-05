e19c45cdb71d4940e4319aab52e3046cc103b3fe
likas <lika.shugurova@gmail.com>
2019-12-06 00:53:45 +0200

    Boring moving of packages

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package com.sia.tacocloud;
+package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sia.tacocloud;
+package tacos;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
